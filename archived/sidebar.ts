d63af8fa21201b6b7d1a9607ca3488b8d12a62e8
Clas Wen <clas.wen@icloud.com>
2025-03-30 21:19:36 +0800

    use base for side bar link

--- archived/sidebar.ts
+++ archived/sidebar.ts
@@ -6,26 +6,26 @@ export const sidebar = [
   {
     text: '2025',
     collapsed: false,
+    base: '/archived/2025/',
     items: [
       {
         text: '会议纪要',
         items: [
           {
             text: '2025.01.25部长会议',
-            link: '/archived/2025/2025.01.25部长会议'
+            link: '2025.01.25部长会议'
           },
           {
             text: '2025.01.26年度总结会议',
-            link: '/archived/2025/2025.01.26年度总结会议'
+            link: '2025.01.26年度总结会议'
           },
           {
             text: '2025.01.25开发部例会',
-            link: '/archived/2025/2025.01.25开发部例会'
+            link: '2025.01.25开发部例会'
           },
         ]
       }
     ]
-
   },
   {
     text: '2024',
@@ -37,42 +37,43 @@ export const sidebar = [
       },
       {
         text: '会议纪要',
+        base: '/archived/2024/meetings/',
         items: [
           {
             text: '2024.01.06例会',
-            link: '/archived/2024/meetings/2024.01.06例会'
+            link: '2024.01.06例会'
           },                // 2024.01.06例会.md
           {
             text: '2024.03.01例会',
-            link: '/archived/2024/meetings/2024.03.01例会'
+            link: '2024.03.01例会'
           },                // 2024.03.01例会.md
           {
             text: '2024.03.16例会',
-            link: '/archived/2024/meetings/2024.03.16例会'
+            link: '2024.03.16例会'
           },                // 2024.03.16例会.md
           {
             text: '2024.04.13例会',
-            link: '/archived/2024/meetings/2024.04.13例会'
+            link: '2024.04.13例会'
           },                // 2024.04.13例会.md
           {
             text: '2024.05.11例会',
-            link: '/archived/2024/meetings/2024.05.11例会'
+            link: '2024.05.11例会'
           },                // 2024.05.11例会.md
           {
             text: '2024.08.07线上会议',
-            link: '/archived/2024/meetings/2024.08.07线上会议'
+            link: '2024.08.07线上会议'
           },                // 2024.08.07线上会议.md
           {
             text: '2024.09.14线上会议',
-            link: '/archived/2024/meetings/2024.09.14线上会议'
+            link: '2024.09.14线上会议'
           },                // 2024.09.14线上会议.md
           {
             text: '2024.10.13例会',
-            link: '/archived/2024/meetings/2024.10.13例会'
+            link: '2024.10.13例会'
           },                // 2024.10.13例会.md
           {
             text: '2024.11.03例会',
-            link: '/archived/2024/meetings/2024.11.03例会'
+            link: '2024.11.03例会'
           },                // 2024.11.03例会.md
         ]
       }
@@ -88,87 +89,89 @@ export const sidebar = [
       },
       {
         text: '开发组',
+        base: '/archived/2023/developer/',
         items: [
           {
             text: '2023.10 开发组新人培养方案',
-            link: '/archived/2023/developer/2023.10.开发组新人培养方案'
+            link: '2023.10.开发组新人培养方案'
           },
           {
             text: '2023.10 聊天室项目设计方案',
-            link: '/archived/2023/developer/2023.10.聊天室项目设计方案'
+            link: '2023.10.聊天室项目设计方案'
           },
           {
             text: '2023.10 开发组规划',
-            link: '/archived/2023/developer/2023.10开发组规划'
+            link: '2023.10开发组规划'
           },
           {
             text: '2023.11 重启计协博客',
-            link: '/archived/2023/developer/2023.11.重启计协博客'
+            link: '2023.11.重启计协博客'
           },
           {
             text: '2023.11 内网服务器事宜',
-            link: '/archived/2023/developer/2023.11.内网服务器事宜'
+            link: '2023.11.内网服务器事宜'
           },
         ]
       },
       {
         text: '会议纪要',
+        base: '/archived/2023/meetings/',
         items: [
           {
             text: '2023.01.31例会',
-            link: '/archived/2023/meetings/2023.01.31例会'
+            link: '2023.01.31例会'
           },          // 2023.03.12集会.md
           {
             text: '2023.03.12集会',
-            link: '/archived/2023/meetings/2023.03.12集会'
+            link: '2023.03.12集会'
           },          // 2023.03.12集会.md
           {
             text: '2023.03.17例会',
-            link: '/archived/2023/meetings/2023.03.17例会'
+            link: '2023.03.17例会'
           },          // 2023.03.17例会.md
           {
             text: '2023.04.01例会',
-            link: '/archived/2023/meetings/2023.04.01例会'
+            link: '2023.04.01例会'
           },          // 2023.04.01例会.md
           {
             text: '2023.04.13同宋光慧老师的交流',
-            link: '/archived/2023/meetings/2023.04.13同宋光慧老师的交流'
+            link: '2023.04.13同宋光慧老师的交流'
           },          // 2023.04.13同宋光慧老师的交流.md
           {
             text: '2023.04.15例会',
-            link: '/archived/2023/meetings/2023.04.15例会'
+            link: '2023.04.15例会'
           },          // 2023.04.15例会.md
           {
             text: '2023.05.07例会',
-            link: '/archived/2023/meetings/2023.05.07例会'
+            link: '2023.05.07例会'
           },          // 2023.05.07例会.md
           {
             text: '2023.05.20例会',
-            link: '/archived/2023/meetings/2023.05.20例会'
+            link: '2023.05.20例会'
           },          // 2023.05.20例会.md
           {
             text: '2023.06.10例会',
-            link: '/archived/2023/meetings/2023.06.10例会'
+            link: '2023.06.10例会'
           },          // 2023.06.10例会.md
           {
             text: '2023.08.05例会',
-            link: '/archived/2023/meetings/2023.08.05例会'
+            link: '2023.08.05例会'
           },          // 2023.08.05例会.md
           {
             text: '2023.09.02例会',
-            link: '/archived/2023/meetings/2023.09.02例会'
+            link: '2023.09.02例会'
           },          // 2023.09.02例会.md
           {
             text: '2023.09.25例会',
-            link: '/archived/2023/meetings/2023.09.25例会'
+            link: '2023.09.25例会'
           },          // 2023.09.25例会.md
           {
             text: '2023.10.21例会',
-            link: '/archived/2023/meetings/2023.10.21例会'
+            link: '2023.10.21例会'
           },          // 2023.10.21例会.md
           {
             text: '2023.11.04例会',
-            link: '/archived/2023/meetings/2023.11.04例会'
+            link: '2023.11.04例会'
           },          // 2023.11.04例会.md
         ]
       }
@@ -180,34 +183,35 @@ export const sidebar = [
     items: [
       {
         text: '会议纪要',
+        base: '/archived/2022/',
         items: [
           {
             text: '2022.08.26例会',
-            link: '/archived/2022/2022.08.26例会'
+            link: '2022.08.26例会'
           },
           {
             text: '2022.09.03例会',
-            link: '/archived/2022/2022.09.03例会'
+            link: '2022.09.03例会'
           },
           {
             text: '2022.09.10例会',
-            link: '/archived/2022/2022.09.10例会'
+            link: '2022.09.10例会'
           },
           {
             text: '2022.10.15例会',
-            link: '/archived/2022/2022.10.15例会'
+            link: '2022.10.15例会'
           },
           {
             text: '2022.10.29例会',
-            link: '/archived/2022/2022.10.29例会'
+            link: '2022.10.29例会'
           },
           {
             text: '2022.11.12例会',
-            link: '/archived/2022/2022.11.12例会'
+            link: '2022.11.12例会'
           },
           {
             text: '2022.11.26例会',
-            link: '/archived/2022/2022.11.26例会'
+            link: '2022.11.26例会'
           }
         ]
       }
